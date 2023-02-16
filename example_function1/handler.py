872e57ae03cdc2fb12d977ec765c03d9b09293e2
chandru4ni <35518353+chandru4ni@users.noreply.github.com>
2023-02-16 17:41:38 +0530

    Update handler.py

--- example_function1/handler.py
+++ example_function1/handler.py
@@ -2,5 +2,6 @@ def handle(data, client):
     print(f"Hello from {__name__}!")
     print("I got the following data:")
     print(data)
+    print("changed done to ex func 1")
     print("Will now return data")
     return data
